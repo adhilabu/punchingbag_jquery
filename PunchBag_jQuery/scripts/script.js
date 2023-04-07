de98a459d506ae0c2cdc2821a5a9ba436b60dfad
adhilabu <124715404+adhilabu@users.noreply.github.com>
2023-04-07 09:57:01 +0530

    Update script.js

--- PunchBag_jQuery/scripts/script.js
+++ PunchBag_jQuery/scripts/script.js
@@ -8,7 +8,6 @@ function punchfunc() {
   var final =
     ((health - 1) / health) * parseInt($("#bag-health div").css("width"), 10);
   $("#bag-health div").css("width", final);
-  console.log(final);
   //condition for zero health
   if (health <= 1) {
     $("#punch, #bag").hide();
